ea903a1f8dbcb88a06cda979ccd80d0f02da8666
universato <universato@gmail.com>
2022-10-30 19:19:17 +0900

    Update fjord.js to 0.1.8

--- fjord.js
+++ fjord.js
@@ -1,8 +1,10 @@
 // ==UserScript==
 // @name         fjord_bootcamp_keyboard_pagination
+// @name:ja      フィヨルドブートキャンプ･キーページネーション
 // @namespace    https://greasyfork.org/ja/users/570127
-// @version      0.1.5
+// @version      0.1.8
 // @description  Fjord Bootcampで左右のキーで移動できるようになります
+// @description:ja  Fjord Bootcampで左右のキーで移動できるようになります
 // @author       universato
 // @license      CC0
 // @match        https://bootcamp.fjord.jp/*
@@ -14,24 +16,24 @@
         const activeTagName = document.activeElement.tagName;
         if (['TEXTAREA', 'INPUT'].includes(activeTagName)){ return; }
 
-        let elements = [];
+        let element;
         const pathnames = location.pathname.split('/');
         const flag0 = pathnames[1] === 'reports' && pathnames[2];
-        const flag1 = pathnames[1] === 'current_user' && ['questions', 'reports', 'watches'].includes(pathnames[2]);
-        const flag2 = ['announcements', 'notifications', 'pages', 'questions', 'reports', 'searchables', 'users', 'events'].includes(pathnames[1]);
+        const flag1 = pathnames[1] === 'current_user' && pathnames[2].match(/^(questions|reports|watches)$/);
+        const flag2 = pathnames[1].match(/^(announcements|events|notifications|pages|questions|reports|searchables|users)$/);
         if (flag0){
             if (event.key === 'ArrowLeft'){
-                elements = document.getElementsByClassName('page-content-prev-next__item-link is-prev');
+                element = document.querySelector('a.page-content-prev-next__item-link.is-prev');
             }else if(event.key === 'ArrowRight'){
-                elements = document.getElementsByClassName('page-content-prev-next__item-link is-next');
+                element = document.querySelector('a.page-content-prev-next__item-link.is-next');
             }
         } else if (flag1 || flag2){
            if (event.key === 'ArrowLeft'){
-                elements = document.getElementsByClassName('pagination__item-link is-prev');
+                element = document.querySelector('a.pagination__item-link.is-prev');
             }else if(event.key === 'ArrowRight'){
-                elements = document.getElementsByClassName('pagination__item-link is-next');
+                element = document.querySelector('a.pagination__item-link.is-next');
             }
         }
-        elements[0]?.click();
+        element?.click();
     }, false);
 })();
