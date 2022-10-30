7d252df35e7158e8c661c6d7c8f1fde3599ddf09
universato <universato@gmail.com>
2022-10-31 03:53:59 +0900

    Update atcoder.js userscript

--- atcoder.js
+++ atcoder.js
@@ -1,25 +1,25 @@
 // ==UserScript==
-// @name         AtCoder_CustomTest_Run_Shortcut
-// @namespace    https://greasyfork.org/ja/users/570127
-// @version      0.1.6
-// @description  AtCoderのコードテストにおいてCtrl+Enterで実行ができるようになります。You can run by using Ctrl + Enter in Custom Test of AtCoder
-// @author       universato
-// @grant        none
-// @match        https://atcoder.jp/contests/*/custom_test
-// @match        https://atcoder.jp/contests/*/custom_tets*
-// @license      MIT
-// @supportURL   https://twitter.com/universato
+// @name           AtCoder_CustomTest_Run_Shortcut
+// @name:ja        AtCoderカスタムテスト実行ショートカット
+// @namespace      https://greasyfork.org/ja/users/570127
+// @version        0.1.11
+// @description    You can run by using Ctrl + Enter in Custom Test of AtCoder
+// @description:ja AtCoderのコードテストにおいてCtrl+Enterで実行ができるようになります。
+// @author         universato
+// @grant          none
+// @match          https://atcoder.jp/contests/*/custom_test
+// @match          https://atcoder.jp/contests/*/custom_test*
+// @license        MIT
+// @supportURL     https://twitter.com/universato
 // ==/UserScript==
+
+const element = document.querySelector('a.btn.btn-primary');
+if(element.innerText.match(/^(Run|実行)$/)){ element.innerText += ' (Ctrl + Enter)'; }
 
 (function() {
   document.addEventListener('keydown', function (event) {
       if ((event.ctrlKey || event.metaKey) && event.key === 'Enter'){
-          const elements = document.getElementsByClassName('btn btn-primary');
-          for(let element of elements){
-              if(element.innerText === '実行'){
-                  element.click();
-              }
-          }
+          document.querySelector('a.btn.btn-primary').click();
       }
   });
 })();
