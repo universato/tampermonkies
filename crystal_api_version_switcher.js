22e45e9ee6feb0f43275f941f4f7c2be57b6e742
universato <universato@gmail.com>
2022-11-11 18:19:33 +0900

    Fix invalid mode for crystal version switcher

--- crystal_api_version_switcher.js
+++ crystal_api_version_switcher.js
@@ -2,7 +2,7 @@
 // @name          Crystal API version switcher
 // @name:ja      Crystal APIバージョンスイッチャー
 // @namespace
-// @version       1.0.0
+// @version       1.0.1
 // @description    You can changel version of Crystal API by key shortcut
 // @description:ja  Crystal APIのバージョンをキーボードのショートカットで切り替えます
 // @author        universato
@@ -18,6 +18,8 @@
   document.addEventListener('keydown', function (event) {
       const activeTagName = document.activeElement.tagName;
       const in_textarea = ['TEXTAREA', 'INPUT'].includes(activeTagName);
+      const invalid_input = event.key.match(/[^lm]/);
+       if(in_textarea || invalid_input){ return false; }
 
       const paths = location.pathname.split('/');
       const current_version = paths[2];
