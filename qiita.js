27b657a2a851860b201fc58249c37fc08e54be48
universato <universato@gmail.com>
2024-07-15 20:11:13 +0900

    qiita.jsを最新版に変更

--- qiita.js
+++ qiita.js
@@ -1,73 +1,72 @@
 // ==UserScript==
 // @name         Qiita記事マークダウンリンク生成
-// @version      0.2
+// @version      2024.7.15
 // @description  Qiita記事へのリンクをMarkdown記法で生成する
 // @author       universato
 // @match        https://qiita.com/*/items/*
 // @match        https://qiita.com/*
 // ==/UserScript==
 
-// fukuchan-sanの動かなくなっていたユーザースクリプトを修正した上で、クリックだけで動作するようにした。
+// fukuchan-sanの動かなくなっていたユーザースクリプトと同じアイディア。
 // 何も表示されないのが最大の欠点だけど、わかってる分には使いやすそう。
+// Create Linkを使えば良さそうだけど。
 
-// Qiita記事リンク生成 https://greasyfork.org/ja/scripts/397378-qiita%E8%A8%98%E4%BA%8B%E3%83%AA%E3%83%B3%E3%82%AF%E7%94%9F%E6%88%90
+// Qiita記事リンク生成 https://greasyfork.org/ja/scripts/397378-qiita記事リンク生成
 
 // JavaScriptでクリップボードにテキストをコピーする - arms inc. Engineers' Blog https://tech.arms-soft.co.jp/entry/2022/07/27/090000
 // JavaScriptでクリップボードにテキストをコピーする(Codepen) https://codepen.io/takayo-nakamura/pen/qBoaVza
 
-"use strict;"
+// さらにキーショートカットでコピーしたり移動できるようにした。
+// ログインユーザー名が取得しにくくなってた。
+
+"use strict";
 
 const paths = location.pathname.split('/');
 
-if(paths[2] === "items"){
-    const span = document.createElement("span");
-    span.classList.add("fa", "fa-link");
+window.onload = function() {
+  // ページがロードされたら実行するコード
 
-    const div = document.createElement("div");
-    div.classList.add("css-ag7bw5");
-    div.append(span);
-    // div.addEventListener("click", () => prompt(document.title, `[${document.title}](${location.href})`));
+  if(paths[2] === "items"){
+    const md_copy_div = document.createElement('div');
+    md_copy_div.textContent = 'MD';
 
-    const container = document.querySelector(".css-yrmhnf");
+    // ここのHTMLタグが変わりやすい。
+    const container = document.querySelector("section.style-1hl01qi");
 
     // navigator.clipboardが使えるときだけ、アイコンを追加する。iOSでは使えないらしい。
     if(navigator.clipboard){
-      container.append(div);
+      container.append(md_copy_div);
     }
 
-    // クリップボードへコピー
-    div.addEventListener('click', () => {
-      const tagValue = `[${document.title}](${location.href})`;
-      navigator.clipboard.writeText(tagValue).then(function () {});
+    md_copy_div.addEventListener('click', () => {
+      const markdown_link = `[${document.title}](${location.href})`;
+      navigator.clipboard.writeText(markdown_link);
     })
-}
+  }
+};
 
 // Shortcuts to switch version by key
 (function() {
-  'use strict';
   document.addEventListener('keydown', function (event) {
       const activeTagName = document.activeElement.tagName;
       const in_textarea = ['TEXTAREA', 'INPUT'].includes(activeTagName);
-      const invalid_input = event.key.match(/[^dgnps]/);
-       if(in_textarea || invalid_input){ return false; }
-
-      if(paths[2] === 'new' || paths[3] === 'edit'){ return false }
-
-      const user_id = document.querySelector('.st-NewHeader_loginUser > img').alt;
-
-     // const paths = location.pathname.split('/');
-      //const current_version = paths[2];
-
+      const invalid_input = event.key.match(/[^mdnps]/);
+      if(in_textarea || invalid_input){ return false; }
       if(event.ctrlKey || event.metaKey){ return false; }
+      if(paths[2] === 'new' || paths[3] === 'edit'){ return false; }
 
-     if(event.key.match(/[ds]/)){
+      if(event.key === 'n'){
+        location.href = 'https://qiita.com/drafts/new';
+      }else if(event.key.match(/[ds]/)){
           // drafts, 下書き(shitagaki)
           location.href = "https://qiita.com/drafts";
-      }else if(event.key.match(/[gp]/)){
+      }else if(event.key.match(/[p]/)){
           // private, 限定公開(gentei kokai)
-          location.href = `https://qiita.com/${user_id}/private`;
-      }else if(event.key === 'n'){
-          location.href = 'https://qiita.com/drafts/new';
+          // const user_id = document.querySelector('[TODO]');
+          // location.href = `https://qiita.com/${user_id}/private`;
+      }else if(event.key === 'm'){
+        const md_link = `[${document.title}](${location.href})`;
+        navigator.clipboard.writeText(md_link);
       }
   }, false);
 })();
